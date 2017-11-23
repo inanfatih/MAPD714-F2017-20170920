0a3511a3c25bd0f54a67c8a0042ad503c8c7a146
Jaismeen <jaismeen.sandhu@gmail.com>
2017-11-22 19:59:24 -0500

    table gosteriyor.

--- Simple Table/Simple Table/ViewController.swift
+++ Simple Table/Simple Table/ViewController.swift
@@ -1,15 +1,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    private let movies = ["Batman vs Superman", "Blade Runner", "Thor", "Justice", "Wonderwoman", "Dark Tower", "Star Wars"]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if (cell == nil)
+        {
+            cell = UITableViewCell(
+                style: UITableViewCellStyle.default,
+                reuseIdentifier: simpleTableIdentifier
+            )
+        }
+        
+        cell?.textLabel?.text=movies[indexPath.row]
+        return cell!
+        
+        
+    }
     
 
 }
